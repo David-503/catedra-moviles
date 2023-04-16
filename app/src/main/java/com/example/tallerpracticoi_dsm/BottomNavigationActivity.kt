eae6fb07fd8cc92216de73a0e81be8407a6e62a2
garay <garayalvarado14@gmail.com>
2023-04-15 22:33:52 -0600

    Realiza merge y cambios en activities

--- app/src/main/java/com/example/tallerpracticoi_dsm/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/tallerpracticoi_dsm/BottomNavigationActivity.kt
@@ -4,43 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.tallerpracticoi_dsm.utils.AppLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class BottomNavigationActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationBar: BottomNavigationView
+class BottomNavigationActivity : AppLayout() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_bottom_navigation)
+        super.onCreate(savedInstanceState, )
 
-        bottomNavigationBar = findViewById(R.id.bottom_navigation_bar)
-
-        bottomNavigationBar.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> {
-                    true
-                }
-
-                R.id.explore -> {
-                    startActivity(Intent(this, OptionTwoActivity::class.java))
-                    overridePendingTransition(0,0)
-                    true
-                }
-
-                R.id.setting -> {
-                    Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show()
-                    true
-                }
-
-                R.id.search -> {
-                    Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> {
-                    Toast.makeText(this, "Unknown option", Toast.LENGTH_SHORT).show()
-                    true
-                }
-            }
-        }
     }
 }
